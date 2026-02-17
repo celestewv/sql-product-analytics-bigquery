d5e9a0e81aa41d53dd2f7b19f3f2b75d21261c70
celestewv <122484285+celestewv@users.noreply.github.com>
2026-02-16 23:05:55 -0800

    Update 01_executive_kpis.sql

--- sql/01_executive_kpis.sql
+++ sql/01_executive_kpis.sql
@@ -1,1 +1,10 @@
+-- 01_executive_kpis.sql
+-- Summary KPIs (Users, Sessions, Transactions, Conversion)
 
+SELECT
+  COUNT(DISTINCT fullVisitorId) AS users,
+  COUNT(*) AS sessions,
+  SUM(totals.pageviews) AS total_pageviews,
+  SUM(totals.transactions) AS total_transactions,
+  SAFE_DIVIDE(SUM(totals.transactions), COUNT(*)) AS transactions_per_session
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
