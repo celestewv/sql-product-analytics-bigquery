da51ac97327f138642d2dfa1fabddee8411c48b4
celestewv <122484285+celestewv@users.noreply.github.com>
2026-02-16 23:07:37 -0800

    Update 03_channel_segmentation.sql

--- sql/03_channel_segmentation.sql
+++ sql/03_channel_segmentation.sql
@@ -1,1 +1,10 @@
+-- Channel Performance Analysis
 
+SELECT
+  trafficSource.medium AS channel,
+  COUNT(*) AS sessions,
+  SUM(IF(totals.transactions >= 1, 1, 0)) AS purchasing_sessions,
+  SAFE_DIVIDE(SUM(IF(totals.transactions >= 1, 1, 0)), COUNT(*)) AS purchase_rate
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+GROUP BY channel
+ORDER BY purchase_rate DESC;
